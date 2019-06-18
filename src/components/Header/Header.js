b91235086d42f8548a5a42726c03bc0b1b172a3f
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2019-06-19 02:21:24 +0530

    add logo image and favicons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,15 @@ import React from "react";
 
 import "./style.css";
 
+const logoImage = require("../../sharedImages/logo.svg");
+
 const Header = () => {
-  return <h2>BookScout</h2>;
+  return (
+    <h2>
+      <img src={logoImage} alt="logo" />
+      BookScout
+    </h2>
+  );
 };
 
 export default Header;
