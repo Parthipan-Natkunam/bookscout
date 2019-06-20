81a3807ee3a89a9a65b80d174fdd1adec51ff697
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2019-06-21 00:24:31 +0530

    handle fallbacks for undefined image and description

--- src/components/Results/ResultCard.js
+++ src/components/Results/ResultCard.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import "./style.css";
 
+const defaultImage = require("../../sharedImages/logo.svg");
+
 const ResultCard = props => {
   //return <div className={"result__card"}>results shown here</div>;
   let { title, subtitle, description, imageLinks } = props.info;
+  description = description || "Description is not available for this title";
+  imageLinks = (imageLinks && imageLinks.smallThumbnail) || defaultImage;
   return (
     <div className={"result__card"}>
-      <img src={imageLinks.smallThumbnail} alt={title} />
+      <img src={imageLinks} alt={title} />
       <h3> {title} </h3>
       <h4>{subtitle}</h4>
       <p className={"result__description"}>{`${description.substring(
